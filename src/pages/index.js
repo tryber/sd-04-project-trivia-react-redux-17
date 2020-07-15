64d3e96a33949eec994267cebcfb33ad404fdf37
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-07-15 19:24:40 -0300

    retornando ao inicio

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Home from '../pages/Home';
-import Feedback from '../pages/Feedback';
-import Game from '../pages/Game';
-import NotFound from '../pages/NotFound';
-import Ranking from '../pages/Ranking';
-import Settings from '../pages/Settings';
-
-export { Home, Feedback, Game, NotFound, Ranking, Settings }
