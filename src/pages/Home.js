1cea85de7c288104ad4f8fbf30f6a92fbd0bb380
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-07-15 19:18:29 -0300

    Iniciando projeto, defiindo rotas, criando pages e components iniciais

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+function Home (){
+  return <div>Home</div>
+}
+
+// const mapStateToProps = (state) => ({
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Home);
+
+export default Home;
---
+++ src/pages/Ranking.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+function Ranking (){
+  return <div>Ranking</div>
+}
+
+// const mapStateToProps = (state) => ({
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Ranking);
+
+export default Ranking
